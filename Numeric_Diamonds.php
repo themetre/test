2c108925780c204a6e91448e9ce8240c79db3432
Mark Gannaway <mark@ganners.co.uk>
2013-07-02 19:49:56 +0100

    Added template for Numeric Diamonds class

---
+++ Numeric_Diamonds.php
@@ -0,0 +1,37 @@
+<?php
+
+class Numeric_Diamonds {
+
+	private $_squareNumber;
+
+	/**
+	 * Our construct - takes our square number we want
+	 * to convert into a diamond.
+	 * 
+	 * @param int $squareNumber - Must be a square number!
+	 * 
+	 */
+	public function __construct($squareNumber) {
+
+	}
+
+	/**
+	 * Generates our diamond string
+	 * 
+	 * @return string
+	 */
+	public function generateDiamond() {
+
+	}
+
+	/**
+	 * Tests if the number is a square number (e.g. 4,
+	 * 9, 16, 25, 36, 49)
+	 * 
+	 * @return bool
+	 */
+	private function _checkNumberIsSquare() {
+
+	}
+
+}
