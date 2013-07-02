c77e1a21066635e276b6dfd44259be34e3a12985
Mark Gannaway <mark@ganners.co.uk>
2013-07-02 19:49:03 +0100

    Added test template for our Numeric_Diamonds class

---
+++ tests/Numeric_DiamondsTest.php
@@ -0,0 +1,41 @@
+<?php
+
+class Numeric_DiamondsTest extends PHPUnit_Framework_TestCase {
+
+	/**
+	 * Sets up our state
+	 */
+	public function setUp() {
+
+	}
+
+	/**
+	 * We test that our construct does indeed
+	 * set the number
+	 */
+    public function testConstruct() {
+
+    }
+
+    /**
+     * Test our check for detecting if a number is square
+     */
+    public function testCheckNumberIsSquare() {
+
+    }
+
+    /**
+     * Test our diamond output
+     */
+    public function testGenerateDiamond() {
+
+    }
+
+    /**
+     * Resets our state
+     */
+    public function tearDown() {
+
+    }
+
+}
