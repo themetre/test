678c62ef47ff4a55046eea680657d69f72c4e876
Graffito <demeter@irish-coffee.(none)>
2013-07-04 01:03:30 +0100

    Entered solutions for test. Carlos Bendfeldt

--- src/Numeric_Diamonds.php
+++ src/Numeric_Diamonds.php
@@ -1,5 +1,4 @@
 <?php
-
 class Numeric_Diamonds {
 
 	private $_squareNumber;
@@ -12,8 +11,9 @@ class Numeric_Diamonds {
 	 * 
 	 */
 	public function __construct($squareNumber) {
-		
+		$this->_squareNumber = $squareNumber;
 	}
+
 
 	/**
 	 * Generates our diamond string
@@ -21,8 +21,48 @@ class Numeric_Diamonds {
 	 * @return string
 	 */
 	public function generateDiamond() {
-
+		$output = "";
+		$blank = " ";
+		$digits = ceil( log10 ( $this->_squareNumber + 1) );
+		$maxdigits = $digits * 2;
+		
+		$root = intval( $this->squareRoot( $this->_squareNumber ) );
+		
+		$right = -1 * $root + 1;
+		
+		for($index = 1; $index < $root * 2; $index++) {
+			$left = ($root <= $index) ? $root * 2 - $index : $index;
+			
+			$right = ($root < $index) ? $right + 1 : $right + $root;
+			
+			$margin = abs($root - $index) * $digits + $digits;
+			$output .= $this->fill($right, $margin, $blank);
+			
+			for($j = 1; $j < $left; $j++) {
+				$current_number = $right - ($root - 1) * $j;
+				$output .= $this->fill($current_number, $maxdigits, $blank); // str_replace(" ", $blank, printf($padding, $current_number));
+			}
+			$output .= $index + 1 < $root * 2? PHP_EOL : "";
+		}
+		
+		return $output;
 	}
+	
+	
+	/**
+	 * Generates our diamond string
+	 * 
+	 * @return string
+	 */
+	private function fill($string, $digits, $spacer = ".") {
+		$padding = "";
+		$max = $digits - strlen($string);
+		for($index = 0; $index < $max; $index++)
+			$padding .= $spacer;
+		
+		return $padding . $string;
+	}
+
 
 	/**
 	 * Tests if the number is a square number (e.g. 4,
@@ -30,8 +70,22 @@ class Numeric_Diamonds {
 	 * 
 	 * @return bool
 	 */
-	private function _checkNumberIsSquare() {
+	private function _checkNumberIsSquare($number) {
+		$squareRoot = $this->squareRoot($number);
 
+		return $squareRoot == floor($squareRoot); 
 	}
+	
+	/**
+	 * Get Square root of number, only 
+	 * 
+	 * @return float
+	 */
+	private function squareRoot($number) {
+		if ( is_numeric( $number ) )
+			return sqrt( doubleval( $number ));
+		
+		return -0.1;
+	}	
 
 }
